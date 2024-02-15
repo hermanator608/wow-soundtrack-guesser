14cf26ca8428d1fa465647f42a728e5dc2186a00
maggiemay-afk <hermsmk@gmail.com>
2024-02-15 15:12:16 -0500

    clean up unused imports, variables, etc.

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,23 @@ import Header from "./components/Header";
 import "./App.css";
 import BackgroundImage from "./components/BackgroundImage";
 import { YoutubePlayer } from "./components/YoutubePlayer";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import {
   currentQuestionState,
   getNextQuestion,
   videoShownState,
 } from "./state";
-import { Button, Card, CardActionArea, CardContent, Grid, Typography, styled } from "@mui/material";
+import { Card, CardActionArea, CardContent, Grid, Typography } from "@mui/material";
 import { worldOfWarcraft } from "./config";
 
-const Flexy = styled("div")`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
 
 function App() {
-  const [currentQuestion, setCurrentQuestion] =
-    useRecoilState(currentQuestionState);
-
-  const [videoShown, setVideoShown] = useRecoilState(videoShownState);
+  const [currentQuestion, setCurrentQuestion] = useRecoilState(currentQuestionState);
+  const setVideoShown = useSetRecoilState(videoShownState);
 
   const onQuestionSelect = (selectedIndex: number) => {
     if (selectedIndex === currentQuestion.answer) {
       alert("Correct!");
-      //
       setVideoShown(true);
 
       setTimeout(() => {
@@ -46,18 +38,18 @@ function App() {
       <Grid container spacing={3} justifyContent="center" alignItems="center">
         {currentQuestion.options.map((q, i) => (
           <Grid item xs={12}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardActionArea onClick={() => onQuestionSelect(q)}>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Option #{i+1}
-              </Typography>
-              <Typography variant="h5" component="div">
-                {worldOfWarcraft[q]?.name}
-              </Typography>
-            </CardContent>
-            </CardActionArea>
-          </Card>
+            <Card sx={{ minWidth: 275 }}>
+              <CardActionArea onClick={() => onQuestionSelect(q)}>
+                <CardContent>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Option #{i + 1}
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    {worldOfWarcraft[q]?.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
           </Grid>
         ))}
       </Grid>
